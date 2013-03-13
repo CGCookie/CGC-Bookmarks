db8a66af736faa8baa24c88107655f26d3c4eb03
Kyle Unzicker <kyle@wndfrm.com>
2013-03-13 11:29:42 -0500

    changed image size

--- includes/list-bookmarks.php
+++ includes/list-bookmarks.php
@@ -8,8 +8,8 @@ function cgc_list_bookmarks( $delete_link = true, $number = 999 ) {
 	global $wpdb;
 	global $cgcb_db_table;
 	global $cgcbbaseDir;
-	
-	
+
+
 	if(is_user_logged_in()) {
 		// echo the delete script
 		if($delete_link == true ) {
@@ -24,7 +24,7 @@ function cgc_list_bookmarks( $delete_link = true, $number = 999 ) {
 					var user_id = $(this).attr('class');
 					var row_id = '.'+$(this).attr('id').replace('remove_', '');
 					var info = 'remove_bookmark=&cgcb_post_url=' + bookmark_url + '&cgcb_user_id=' + user_id;
-					
+
 					if(confirm('Do you really want to remove this favorite?')) {
 						$.ajax({
 							type: "POST",
@@ -41,14 +41,14 @@ function cgc_list_bookmarks( $delete_link = true, $number = 999 ) {
 						});
 					}
 					return false;
-				});			
+				});
 
 			}); // end jquery(function($))
-			//]]> 
-		</script>	
+			//]]>
+		</script>
 		<?php
 		}
-		
+
 		$display .= '<div class="cgc_bookmarks_list bookmarked_images">';
 		$display .= '<h3>Fav Images</h3>';
 		$display .= '<p>These are your favorite eye-candy.</p>';
@@ -69,7 +69,7 @@ function cgc_list_bookmarks( $delete_link = true, $number = 999 ) {
 				$display .= '<li>You do not have any favorited images.</li>';
 			}
 		$display .= '</ul></div>';
-		
+
 		$display .= '<div class="cgc_bookmarks_list bookmarked_tutorials">';
 		$display .= '<h3>Fav Tutorials</h3>';
 		$display .= '<p>Tutorials you\'ve favorited across the CG Cookie Network.</p>';
@@ -81,7 +81,7 @@ function cgc_list_bookmarks( $delete_link = true, $number = 999 ) {
 					$blog = strrchr($bookmark->post_url, '.com');
 					$blog = substr($blog, 5);
 					$blog = cgcb_rstrstr($blog, '/');
-					
+
 					$image = $bookmark->image_url != '' ? $bookmark->image_url : get_bloginfo("stylesheet_directory") . '/images/oldImage_message.jpg';
 
 					$display .= '<li class="bookmark-link bookmark_' . $bookmark->id . '">';
@@ -90,27 +90,27 @@ function cgc_list_bookmarks( $delete_link = true, $number = 999 ) {
 							$display .= '<span class="cgc_bookmark_delete"> - <a id="remove_bookmark_' . $bookmark->id . '" class="' . $bookmark->user_id . '" href="' .$bookmark->post_url . '">remove</a></span>';
 						}
 					$display .= '</li>';
-			
+
 				}
 			} else {
 				$display .= '<li>You do not have any bookmarked tutorials.</li>';
 			}
 		$display .= '</ul></div>';
-		
+
 	}
 	else {
 		$display .= 'You must be logged in to view your bookmarks.';
 	}
-	
+
 	return $display;
-	
+
 }
 function cgc_get_bookmarked_image_author( $bookmark_url, $bookmark_title ) {
 	global $wpdb;
 
 	if( empty( $bookmark_url ) || empty( $bookmark_title ) ) {
 		return;
-	} 
+	}
 
 	// Get the network slug
 	$find_bookmark_origin_network = explode('/files/', $bookmark_url);
@@ -128,7 +128,7 @@ function cgc_get_bookmarked_image_author( $bookmark_url, $bookmark_title ) {
 
 	// Display the author
 	$bookmark_author = get_the_author_meta( 'display_name', $author_id );
-	
+
 	restore_current_blog();
 
 	return $bookmark_author;
@@ -139,9 +139,9 @@ function cgc_list_bookmarked_images( $number = 999, $list_view = false ) {
 	global $user_ID;
 	global $wpdb;
 	global $cgcb_db_table;
-	
+
 	$display = '';
-	
+
 	if(is_user_logged_in()) {
 		$format = 'grid-view';
 		if ( $list_view ) {
@@ -155,7 +155,7 @@ function cgc_list_bookmarked_images( $number = 999, $list_view = false ) {
 					$image = $bookmark->image_url != '' ? $bookmark->image_url : get_bloginfo("stylesheet_directory") . '/images/image_missing.jpg';
 					$display .= '<li class="bookmark-link bookmarked-image bookmark_' . $bookmark->id . '">';
 						$display .= '<a href="' . $bookmark->post_url . '" title="' . stripslashes($bookmark->post_title) . '" class="favorited-image"><img src="' . $image . '" class="image_bookmark"/></a>';
-						if ( $list_view ) {	
+						if ( $list_view ) {
 							$bookmark_url = $bookmark->image_url;
 							$bookmark_title = $bookmark->post_title;
 							$display .= '<div class="favorited-image-info">';
@@ -169,25 +169,25 @@ function cgc_list_bookmarked_images( $number = 999, $list_view = false ) {
 				$display .= '<li>You do not have any favorited images.</li>';
 			}
 		$display .= '</ul>';
-		
+
 	}
 	else {
 		$display .= 'You must be logged in to view your bookmarks.';
 	}
-	
+
 	return $display;
-	
+
 }
 
 function cgc_list_bookmarked_posts($number = 999 ) {
 	global $user_ID;
 	global $wpdb;
 	global $cgcb_db_table;
-	
+
 	$display = '';
-	
+
 	if(is_user_logged_in()) {
-		
+
 		$display .= '<ul class="bookmarked-posts-list">';
 			$bookmarks = $wpdb->get_results("SELECT * FROM " . $cgcb_db_table . " WHERE (user_id='" . $user_ID . "' AND post_url NOT LIKE '%images/%') ORDER BY id DESC LIMIT $number;");
 			if($bookmarks) {
@@ -198,25 +198,25 @@ function cgc_list_bookmarked_posts($number = 999 ) {
 					$blog = $blog[3];
 
 					if(strlen($bookmark->post_title) > 40) {
-						$title = substr($bookmark->post_title, 0, 40) . '...';	
+						$title = substr($bookmark->post_title, 0, 40) . '...';
 					} else {
-						$title = $bookmark->post_title;	
+						$title = $bookmark->post_title;
 					}
 					$display .= '<li class="bookmark-link bookmark_' . $bookmark->id . ' '. $blog . '"">';
 						$display .= '<a href="' . $bookmark->post_url . '"><span>' . stripslashes($title) . '</span></a>';
 					$display .= '</li>';
-			
+
 				}
 			} else {
 				$display .= '<li>You do not have any favorited tutorials.</li>';
 			}
 		$display .= '</ul>';
-		
+
 	}
 	else {
 		$display .= 'You must be logged in to view your favorites.';
 	}
-	
+
 	return $display;
-	
+
 }
