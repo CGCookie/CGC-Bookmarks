b3cdc18836c8807d93639bb9731f22a5476fb641
Bearded Avenger <email@nickhaskins.com>
2015-05-05 11:31:31 -0500

    gut

---
+++ cgc-bookmarks.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ *
+ * @package   CGC_Bookmarks
+ * @author    Nick Haskins <nick@cgcookie.com>
+ * @license   GPL-2.0+
+ * @link      http://cgcookie.com
+ * @copyright 2014 CG Cookie Inc
+ *
+ * @wordpress-plugin
+ * Plugin Name:       CGC Bookmarks
+ * Plugin URI:        http://cgcookie.com
+ * Description:       A global bookmarking system
+ * Version:           1.0
+ * Author:            Nick Haskins
+ * Author URI:        http://cgcookie.com
+ * Text Domain:       cgc-flows-locale
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Domain Path:       /languages
+ * GitHub Plugin URI: https://github.com/cgcookie/cgc-bookmarks
+ * WordPress-Plugin-Boilerplate: v2.6.1
+ */
